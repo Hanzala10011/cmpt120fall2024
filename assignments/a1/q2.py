2cc002ea1859c60fa653c4592581e3043f2d23ac
Hanzala10011 <hanzalayousuf45@gmail.com>
2024-09-04 11:08:47 -0700

    Update q2.py

--- assignments/a1/q2.py
+++ assignments/a1/q2.py
@@ -1,9 +1,14 @@
 # q2.py
 
 #
-# Full Name:
-#  SFU ID #:
-# SFU Email:
+# Full Name: Hanzala Yousuf     
+#  SFU ID #: 301623949
+# SFU Email: hym7@sfu.ca
 #
 
 # ... put your answer to question 2 here ...
+
+Length_in_feet = input("What is the length in Feet? ")
+length_in_meters = float(Length_in_feet) * 0.3048
+
+print(f"The length in meters is {length_in_meters}")
