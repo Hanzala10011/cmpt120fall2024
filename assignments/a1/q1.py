b1d751b98e262dbb323de54770538b3312ddc5b2
Hanzala10011 <hanzalayousuf45@gmail.com>
2024-09-04 11:03:06 -0700

    Update q1.py

--- assignments/a1/q1.py
+++ assignments/a1/q1.py
@@ -1,9 +1,14 @@
 # q1.py
 
 #
-# Full Name:
-#  SFU ID #:
-# SFU Email:
-#
+# Full Name: Hanzala Yousuf Mahmood    
+#  SFU ID #: 301623949
+# SFU Email: hym7@sfu.ca
 
 # ... put your answer to question 1 here ...
+
+name = input("What is your name? ")
+age = input("how old are you? ")
+age_in_25_years = int(age) + 25
+
+print(f"Hello {name} you are {age} and in 25 years you will be {age_in_25_years}")
